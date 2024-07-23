abb146c9d7b333f1c262f5c9ac0eca11b921f1cf
blackskull01022 <felipemartins033@hotmail.com>
2024-07-23 16:59:45 -0300

    atualização

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import './App.css';
-import './components/HellowWorld'
-import HelloWorld from './components/HellowWorld';
-
+import './App.css'
+import HelloWorld from './components/HellowWorld'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 
 function App() {
 
-const name = 'Felipe'
-
-const newName = name.toUpperCase()
-
-function sum(a, b){
-  return a + b
-}
-
 const url = 'https://via.placeholder.com/150'
 
+const name = 'Maria'
   return (
+    
     <div className="App">
-     <h1>Ola React!</h1>
-     <p>Olá, {newName}</p>
-     <p>soma: {sum(1, 2)} </p>
-    <img src={url} alt="Minha Imagem"/>
     < HelloWorld/>
-
+    <SayMyName name="Felipe"/>
+    <SayMyName name="Jéssica"/>
+    <SayMyName name={name}/>
+    <Pessoa name='Felipe' idade='23' profissao='Motorista' foto= 'https://via.placeholder.com/150'/>
     </div>
-  );
+  )
 }
 
 export default App;
