36a4db4b8fe0c42a09a160422504a2489aac9dfa
blackskull01022 <felipemartins033@hotmail.com>
2024-07-24 16:49:05 -0300

    Atualizaçao da aula

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css'
 import HelloWorld from './components/HellowWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
-
+import Frase from './components/Frase'
+import List from './components/List'
 
 function App() {
 
@@ -10,13 +11,15 @@ const url = 'https://via.placeholder.com/150'
 
 const name = 'Maria'
   return (
-    
     <div className="App">
-    < HelloWorld/>
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
     <SayMyName name="Felipe"/>
     <SayMyName name="Jéssica"/>
     <SayMyName name={name}/>
     <Pessoa name='Felipe' idade='23' profissao='Motorista' foto= 'https://via.placeholder.com/150'/>
+    <List/>
     </div>
   )
 }
